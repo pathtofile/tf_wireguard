e78d808c09f4debb3ce677afdff0e61f76fdfd0d
pathtofile <path@tofile.dev>
2022-03-22 11:19:04 +1100

    fixed azure script

--- azure/azure.tf
+++ azure/azure.tf
@@ -218,7 +218,7 @@ output "username" {
   value = var.admin_username
 }
 output "ip_address" {
-  value = azurerm_linux_virtual_machine.tf_vm.*.public_ip_address
+  value = azurerm_linux_virtual_machine.tf_vm.public_ip_address
 }
 output "ssh_port" {
   value = var.ssh_port

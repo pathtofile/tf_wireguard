52e4017d9f18ad1b2a4dda638c0579fa5f635ddf
pathtofile <path@tofile.dev>
2022-03-24 13:32:52 +1100

    a

--- azure/azure.tf
+++ azure/azure.tf
@@ -18,7 +18,6 @@ variable "ssh_key_pub" { default = "~/.ssh/id_rsa.pub" }
 variable "ssh_port" { default = 22 }
 
 # Wireguard settings:
-variable "wg_subnet" { default = "10.77.67.1/24" }
 variable "wg_port" { default = 51820 }
 variable "wg_client_pubkey" { type = string }
 variable "wg_psk" {
@@ -203,7 +202,6 @@ resource "azurerm_linux_virtual_machine" "tf_vm" {
       admin_username   = var.admin_username,
       admin_ssh_pubkey = file(var.ssh_key_pub),
       ssh_port         = var.ssh_port,
-      wg_subnet        = var.wg_subnet,
       wg_port          = var.wg_port,
       public_iface     = var.public_iface
   }))
